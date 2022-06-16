67f1e360d7c5e8368ad009d9b9a480c86f56fc21
Melquisedeque de Sousa Pereira <melquisedeque.pereira@sigtrans.com.br>
2022-06-16 14:00:41 -0300

    list products

---
+++ domain/product.go
@@ -0,0 +1,8 @@
+package domain
+
+type Product struct {
+	ID                int
+	Name, Description string
+	Price             int
+	Quantity          int
+}
--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"html/template"
 	"net/http"
 	"path"
+
+	"github.com/MelkdeSousa/ghopping/domain"
 )
 
 var TEMPLATES = template.Must(template.ParseGlob(path.Join("views", "*.html")))
@@ -14,5 +16,11 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	TEMPLATES.ExecuteTemplate(w, "index", nil)
+	products := []domain.Product{
+		{ID: 1, Name: "Laptop", Description: "Laptop description", Price: 1000, Quantity: 134},
+		{ID: 2, Name: "Mouse", Description: "Mouse description", Price: 100, Quantity: 167},
+		{ID: 3, Name: "Keyboard", Description: "Keyboard description", Price: 200, Quantity: 178},
+	}
+
+	TEMPLATES.ExecuteTemplate(w, "index", products)
 }
