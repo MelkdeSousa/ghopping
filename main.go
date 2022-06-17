3b4d05e6eb5fe4ad42fb61c19b5f7dd3955615e8
Melquisedeque de Sousa Pereira <melquisedeque.pereira@sigtrans.com.br>
2022-06-17 15:56:22 -0300

    entrypoint app

--- main.go
+++ main.go
@@ -1,26 +1,12 @@
 package main
 
 import (
-	"html/template"
 	"net/http"
-	"path"
 
-	"github.com/MelkdeSousa/ghopping/domain"
+	"github.com/MelkdeSousa/ghopping/routes"
 )
 
-var TEMPLATES = template.Must(template.ParseGlob(path.Join("views", "*.html")))
-
 func main() {
-	http.HandleFunc("/", index)
+	routes.LoadRoutes()
 	http.ListenAndServe(":8080", nil)
-}
-
-func index(w http.ResponseWriter, r *http.Request) {
-	products := []domain.Product{
-		{ID: 1, Name: "Laptop", Description: "Laptop description", Price: 1000, Quantity: 134},
-		{ID: 2, Name: "Mouse", Description: "Mouse description", Price: 100, Quantity: 167},
-		{ID: 3, Name: "Keyboard", Description: "Keyboard description", Price: 200, Quantity: 178},
-	}
-
-	TEMPLATES.ExecuteTemplate(w, "index", products)
 }
